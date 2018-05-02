a9e1cccf8e57d531782807a0dcc8fe9079c1da7b
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-02 16:23:01 -0400

    added state to App component

--- myFirstReactApp/src/App.js
+++ myFirstReactApp/src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+    state = {
+    person: [
+      { name: "Bay", age: 28},
+      { name: "Andrew", age: 29},
+      { name: "Katniss", age: 2},
+      { name: "Sammy", age: 10}
+    ]
+  }
   
   render() {
     return (
       <div className="App">
         <h1>Hi I am React App</h1>
         <p>Working</p>
-        <Person name="Bay" age="28" />
-        <Person name="Andrew" age="29" />
-        <Person name="Katniss" age="2">My Hobbies: Playing with toys</Person>
-        <Person name="Sammy" age="10" />
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+        <Person name={this.state.person[1].name} age={this.state.person[1].age} />
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}>My Hobbies: Playing with toys</Person>
+        <Person name={this.state.person[3].name} age={this.state.person[3].age} />
       </div>
     );
   }
