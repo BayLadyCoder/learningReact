ac59c8c785b7e830f68607e0adc2b76913b4680e
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-03 13:48:40 -0400

    added switch name button

--- myFirstReactApp/src/App.js
+++ myFirstReactApp/src/App.js
@@ -10,14 +10,30 @@ class App extends Component {
       { name: "Andrew", age: 29},
       { name: "Katniss", age: 2},
       { name: "Sammy", age: 10}
-    ]
+    ],
+
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+    //DON'T DO THIS: this.state.persons[0].name = 'Rachada';
+    this.setState({
+      person: [
+        { name: "Rachada", age: 28},
+        { name: "Andrew", age: 29},
+        { name: "Katniss Everdeen", age: 2},
+        { name: "Sammy Sam", age: 99}
+      ] 
+    })
   }
   
   render() {
     return (
       <div className="App">
-        <h1>Hi I am React App</h1>
-        <p>Working</p>
+        <h1>Hi! This is my first React App</h1>
+        <p>It is Working!!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age} />
         <Person name={this.state.person[1].name} age={this.state.person[1].age} />
         <Person name={this.state.person[2].name} age={this.state.person[2].age}>My Hobbies: Playing with toys</Person>
