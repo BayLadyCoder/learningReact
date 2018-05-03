098844449e5862b3cebee595e50d99de041a5e25
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-03 14:19:59 -0400

    added a parameter, newName, in switchNameHandler method and used .bind to pass the argument to the method.

--- myFirstReactApp/src/App.js
+++ myFirstReactApp/src/App.js
@@ -15,12 +15,12 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
-    console.log('Was clicked!');
+  switchNameHandler = (newName) => {
+    //console.log('Was clicked!');
     //DON'T DO THIS: this.state.persons[0].name = 'Rachada';
     this.setState({
       person: [
-        { name: "Rachada", age: 28},
+        { name: newName, age: 28},
         { name: "Andrew", age: 29},
         { name: "Katniss Everdeen", age: 2},
         { name: "Sammy Sam", age: 99}
@@ -33,11 +33,11 @@ class App extends Component {
       <div className="App">
         <h1>Hi! This is my first React App</h1>
         <p>It is Working!!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, "Rachada!!!")}>Switch Name</button>
         <Person
         name={this.state.person[0].name}
         age={this.state.person[0].age}
-        click={this.switchNameHandler} />
+        click={this.switchNameHandler.bind(this, "BAY!!!!")} />
         <Person
         name={this.state.person[1].name}
         age={this.state.person[1].age} />
