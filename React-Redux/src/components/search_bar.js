5853400996b2578dba68535f35f0263342e12f98
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-07 23:09:18 -0400

    added SearchBar component

---
+++ React-Redux/src/components/search_bar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// use { Component } from 'react' is same as
+// const Component = React.Component
+
+
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
