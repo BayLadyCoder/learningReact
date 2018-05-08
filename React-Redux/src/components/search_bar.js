424dfea985f9b49d2ac4c02cf7371163efcd4324
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-07 23:38:31 -0400

    added state and onChange event handler in SearchBar.

--- React-Redux/src/components/search_bar.js
+++ React-Redux/src/components/search_bar.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 // use { Component } from 'react' is same as
 // const Component = React.Component
 
-
 class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+  }
+
+  // Main method
   render() {
-    return <input />;
+    return (
+      <div>
+        <input onChange={ event => this.setState({ term: event.target.value})} />
+        Value of the input: {this.state.term}
+      </div>
+    )
   }
 }
 
