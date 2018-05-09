85848e24b92c95474561e8b9c4fdafff33ce12a0
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-09 12:27:26 -0400

    added a key to each videoItem using etag property from each video.

--- React-Redux/src/components/video_list.js
+++ React-Redux/src/components/video_list.js
@@ -4,12 +4,13 @@ import VideoListItem from './video_list_item';
 const VideoList = (props) => {
 
   const videoItem = props.videos.map((video) => {
-    return <VideoListItem video={video} />
+    return <VideoListItem key={video.etag} video={video} />
   })
 
   return (
     <ul className="col-md-4 list-group">
     {videoItem}
+    console.log(props.videos)
     </ul>
   );
 };
