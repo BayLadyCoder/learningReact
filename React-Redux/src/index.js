c396227b1ba12cd1fb46f3b0454ab8e525eea80a
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-08 23:40:09 -0400

    Passing value from App's state to VideoList's props.

--- React-Redux/src/index.js
+++ React-Redux/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDP7Y7wKSkXSYKViq3qgOfC1TZLbq-guWI';
 
@@ -25,6 +26,7 @@ class App extends Component {
 		return (
 			<div>
 			<SearchBar />
+			<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
