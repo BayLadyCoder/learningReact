ff97a886812958d047a2dbf3e608b705626a925c
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-10 11:53:44 -0400

    added function to use input value as a search term state in App.

--- React-Redux/src/index.js
+++ React-Redux/src/index.js
@@ -18,21 +18,27 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch ({key: API_KEY, term: 'surfboards'},(videos) => {
+		this.videoSearch('web development');
+	}
+
+	// New method
+	videoSearch(term) {
+		YTSearch ({key: API_KEY, term: term},(videos) => {
 			this.setState({
 				videos: videos,
 				selectedVideo: videos[0]
 			});
-
-
-			console.log(videos);
 		});
 	}
 
+
 	render() {
+
+
+
 		return (
 			<div>
-			<SearchBar />
+			<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
 			<VideoDetail video={this.state.selectedVideo} />
 			<VideoList
 				onVideoSelect={selectedVideo => this.setState({selectedVideo})}
