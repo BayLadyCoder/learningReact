3df9cb54469bfc705638b2cfb2f11432a5e75c8a
BayLadyCoder <rachada_32@hotmail.com>
2019-08-03 21:13:24 -0400

    -

--- React-Redux/src/index.js
+++ React-Redux/src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 
@@ -7,7 +9,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyDP7Y7wKSkXSYKViq3qgOfC1TZLbq-guWI';
+const API_KEY = '';
 
 //Create a new component. This component should produce some HTML
 class App extends Component {
@@ -24,7 +26,10 @@ class App extends Component {
 
 	// New method
 	videoSearch(term) {
-		YTSearch ({key: API_KEY, term: term},(videos) => {
+		YTSearch({
+			key: API_KEY,
+			term: term
+		}, (videos) => {
 			this.setState({
 				videos: videos,
 				selectedVideo: videos[0]
@@ -35,19 +40,34 @@ class App extends Component {
 
 	render() {
 
-		const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 500);
+		const videoSearch = _.debounce((term) => {
+			this.videoSearch(term)
+		}, 500);
 
-		return (
-			<div>
-			<SearchBar onSearchTermChange={videoSearch} />
-			<VideoDetail video={this.state.selectedVideo} />
-			<VideoList
-				onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-				videos={this.state.videos} />
-			</div>
+		return ( <
+			div >
+			<
+			SearchBar onSearchTermChange = {
+				videoSearch
+			}
+			/> <
+			VideoDetail video = {
+				this.state.selectedVideo
+			}
+			/> <
+			VideoList onVideoSelect = {
+				selectedVideo => this.setState({
+					selectedVideo
+				})
+			}
+			videos = {
+				this.state.videos
+			}
+			/> <
+			/div>
 		);
 	}
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector(".container"));
+ReactDOM.render( < App / > , document.querySelector(".container"));
